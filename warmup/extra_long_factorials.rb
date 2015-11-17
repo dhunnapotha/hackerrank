c28fe0e7c29dfc2cf95b2ec2eeef3d14e3560cdd
dhunnapotha <gautam4you@gmail.com>
2015-11-17 14:28:10 +0530

    Cleaned up a bit  more

--- warmup/extra_long_factorials.rb
+++
@@ -1,9 +0,0 @@
-# https://www.hackerrank.com/challenges/extra-long-factorials
-n = gets.chomp.to_i
-
-fact = 1
-for i in 1..n
-  fact *= i
-end
-
-puts fact
--- warmup/staircase.rb
+++
@@ -1,6 +0,0 @@
-# https://www.hackerrank.com/challenges/staircase
-n = gets.chomp.to_i
-
-for i in 1..n
-  printf "%#{n}s\n","#"*i
-end
